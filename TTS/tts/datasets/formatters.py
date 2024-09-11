edb89bca8d2687bccfbfd174d15a6ed53c2fb484
AntonieV <antonie.vietor@gmx.de>
2024-09-11 02:29:27 +0200

    added custom formatters

--- TTS/tts/datasets/formatters.py
+++ TTS/tts/datasets/formatters.py
@@ -12,6 +12,44 @@ from tqdm import tqdm
 # DATASETS
 ########################
 
+def format_avi_dataset(root_path, meta_file, **kwargs):
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    speaker_name = "avi"
+    with open(txt_file, "r", encoding="utf-8") as ttf:
+        for line in ttf:
+            cols = line.split("|")
+            wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
+            text = cols[1]
+            items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
+    return items
+
+
+def format_zs_dataset(root_path, meta_file, **kwargs):
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    speaker_name = "zs"
+    with open(txt_file, "r", encoding="utf-8") as ttf:
+        for line in ttf:
+            cols = line.split("|")
+            wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
+            text = cols[1]
+            items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
+    return items
+
+
+def format_kitt_dataset(root_path, meta_file, **kwargs):
+    txt_file = os.path.join(root_path, meta_file)
+    items = []
+    speaker_name = "kitt"
+    with open(txt_file, "r", encoding="utf-8") as ttf:
+        for line in ttf:
+            cols = line.split("|")
+            wav_file = os.path.join(root_path, "wavs", cols[0] + ".wav")
+            text = cols[1]
+            items.append({"text": text, "audio_file": wav_file, "speaker_name": speaker_name, "root_path": root_path})
+    return items
+
 
 def cml_tts(root_path, meta_file, ignored_speakers=None):
     """Normalizes the CML-TTS meta data file to TTS format
